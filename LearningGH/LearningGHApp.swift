7dc9ae38736e6e2e9b248ea1785949b667385548
osavelyev <osavelyev@gmail.com>
2022-04-04 09:23:31 +0300

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Oleg Savelyev on 04.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
